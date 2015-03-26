b1d58163cc47de78601135633136ed4463ca8fa7
Yonah <joshblour@hotmail.com>
2015-03-26 12:11:07 +0100

    show all projects on home page

--- app/assets/javascripts/app/project.js
+++ app/assets/javascripts/app/project.js
@@ -1,3 +1,52 @@
+App.addChild('ShowProjects', _.extend({
+  el: 'body[data-action="index"][data-controller-name="projects"]',
+
+
+  activate: function(){
+    this.$loader = this.$("#loading img");
+    this.$loaderDiv = this.$("#loading");
+    this.$results = this.$(".results");
+    this.path = this.$("#explore_results").data('projects-path');
+
+
+    this.setInitialFilter();
+    this.setupScroll();
+
+    if(window.location.hash == ''){
+      this.fetchPage();
+
+    }
+  },
+
+
+
+  followRoute: function(route, name, params){
+    this.filter = {};
+    if(params.length > 0){
+      this.filter[name] = params[0];
+    }
+    else{
+      this.filter[name] = true;
+    }
+    this.firstPage();
+    this.fetchPage();
+    if(this.parent && this.parent.$search.length > 0){
+      this.parent.$search.val('');
+    }
+    this.selectLink();
+  },
+
+  setInitialFilter: function(){
+      this.filter = {
+        recommended: true,
+        // not_expired: true
+      };
+  },
+
+}, Skull.InfiniteScroll));
+
+
+
 App.addChild('Project', _.extend({
   el: 'body[data-action="show"][data-controller-name="projects"]',
 
