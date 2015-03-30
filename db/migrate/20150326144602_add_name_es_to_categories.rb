e4fce390728145ffc0561cd33eabdb384cefe8c2
Yonah <joshblour@hotmail.com>
2015-03-30 10:56:21 +0200

    added catalan translations and a language picker

--- db/migrate/20150326144602_add_name_es_to_categories.rb
+++ db/migrate/20150326144602_add_name_es_to_categories.rb
@@ -1,11 +1,14 @@
 class AddNameEsToCategories < ActiveRecord::Migration
   def up
     add_column :categories, :name_es, :string
+    add_column :categories, :name_ca, :string
     Category.all.each do |c|
       c.update_attribute(:name_es, c.name_en)
+      c.update_attribute(:name_ca, c.name_en)
     end
   end
   def down
-    add_column :categories, :name_es, :string
+    remove_column :categories, :name_es, :string
+    remove_column :categories, :name_ca, :string
   end
 end
